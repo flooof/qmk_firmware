1acb88206ee461420bcc7b7d19e3b39e8c0b7b71
flooof <DuyN4c@outlook.com>
2022-09-16 15:39:08 +0700

    add vial to v1

---
+++ keyboards/2x2test/v1/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAD, 0xDC, 0x5C, 0xDE, 0x92, 0xBA, 0x8A, 0x3D}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0, 1, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1, 0, 1}
